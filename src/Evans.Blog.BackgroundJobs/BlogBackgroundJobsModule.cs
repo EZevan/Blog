6f484dd323596244c1812a41d523436104f6c752
evan.zhang <evan_zhang_zwq@outlook.com>
2021-05-10 20:34:00 +0800

    Add background jobs

---
+++ src/Evans.Blog.BackgroundJobs/BlogBackgroundJobsModule.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Volo.Abp.BackgroundJobs.Hangfire;
+using Volo.Abp.Modularity;
+
+namespace Evans.Blog.BackgroundJobs
+{
+    [DependsOn(typeof(AbpBackgroundJobsHangfireModule))]
+    public class BlogBackgroundJobsModule : AbpModule
+    {
+    }
+}
